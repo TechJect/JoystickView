f8aa5be9512807366019b192ca73c80ee621f2a0
Julian Knight <charles@rabidaudio.com>
2014-08-11 13:15:38 -0400

    Our changes: Remove outercircle remove angle calculations allow 9-position snaps more efficient onDraw allow square or circular limits

--- src/com/zerokol/views/JoystickView.java
+++ src/com/zerokol/views/JoystickView.java
@@ -8,105 +8,100 @@ import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 
+import org.jetbrains.annotations.NotNull;
+
 public class JoystickView extends View implements Runnable {
-	// Constants
-	private final double RAD = 57.2957795;
-	public final static long DEFAULT_LOOP_INTERVAL = 100; // 100 ms
-	public final static int FRONT = 3;
-	public final static int FRONT_RIGHT = 4;
-	public final static int RIGHT = 5;
-	public final static int RIGHT_BOTTOM = 6;
-	public final static int BOTTOM = 7;
-	public final static int BOTTOM_LEFT = 8;
-	public final static int LEFT = 1;
-	public final static int LEFT_FRONT = 2;
-	// Variables
-	private OnJoystickMoveListener onJoystickMoveListener; // Listener
+
+	public final static long DEFAULT_LOOP_INTERVAL = 100; //ms
+    public final static int CIRCLE_COLOR = Color.rgb(51, 181, 229);
+    public final static int OUTPUT_SCALE = 32767;
+
+    public static final int TOP = -1;
+    public static final int BOTTOM = 1;
+    public static final int CENTER = 0;
+    public static final int LEFT = -1;
+    public static final int RIGHT = 1;
+
+
+	private OnJoystickMoveListener onJoystickMoveListener;
 	private Thread thread = new Thread(this);
 	private long loopInterval = DEFAULT_LOOP_INTERVAL;
-	private int xPosition = 0; // Touch x position
-	private int yPosition = 0; // Touch y position
-	private double centerX = 0; // Center view x position
-	private double centerY = 0; // Center view y position
-	private Paint mainCircle;
-	private Paint secondaryCircle;
+	private int xPosition = 0;
+	private int yPosition = 0;
+	private double centerX = 0;
+	private double centerY = 0;
+	private Paint innerCircle;
 	private Paint button;
 	private Paint horizontalLine;
 	private Paint verticalLine;
 	private int joystickRadius;
 	private int buttonRadius;
-	private int lastAngle = 0;
-	private int lastPower = 0;
+    private int snapX = CENTER;
+    private int snapY = CENTER;
+    private boolean limitCircular = true;
 
 	public JoystickView(Context context) {
 		super(context);
+        initJoystickView();
 	}
-
 	public JoystickView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		initJoystickView();
 	}
-
 	public JoystickView(Context context, AttributeSet attrs, int defaultStyle) {
 		super(context, attrs, defaultStyle);
 		initJoystickView();
 	}
 
 	protected void initJoystickView() {
-		mainCircle = new Paint(Paint.ANTI_ALIAS_FLAG);
-		mainCircle.setColor(Color.WHITE);
-		mainCircle.setStyle(Paint.Style.FILL_AND_STROKE);
-
-		secondaryCircle = new Paint();
-		secondaryCircle.setColor(Color.GREEN);
-		secondaryCircle.setStyle(Paint.Style.STROKE);
+		innerCircle = new Paint();
+		innerCircle.setColor(CIRCLE_COLOR);
+		innerCircle.setStyle(Paint.Style.STROKE);
+        innerCircle.setStrokeWidth(2);
 
 		verticalLine = new Paint();
-		verticalLine.setStrokeWidth(5);
-		verticalLine.setColor(Color.RED);
+		verticalLine.setStrokeWidth(2);
+		verticalLine.setColor(Color.BLACK);
 
 		horizontalLine = new Paint();
 		horizontalLine.setStrokeWidth(2);
 		horizontalLine.setColor(Color.BLACK);
 
 		button = new Paint(Paint.ANTI_ALIAS_FLAG);
-		button.setColor(Color.RED);
+		button.setColor(CIRCLE_COLOR);
 		button.setStyle(Paint.Style.FILL);
 	}
 
-	@Override
-	protected void onFinishInflate() {
-	}
+    @Override
+    protected void onSizeChanged(int newWidth, int newHeight, int oldWidth, int oldHeight) {
+        super.onSizeChanged(newWidth, newHeight, oldWidth, oldHeight);
+        centerX = (newWidth) / 2;
+        centerY = (newHeight) / 2;
+        resetPosition();
+    }
 
-	@Override
+    @Override
 	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-		// setting the measured values to resize the view to a certain width and
-		// height
+		//always square
 		int d = Math.min(measure(widthMeasureSpec), measure(heightMeasureSpec));
-
 		setMeasuredDimension(d, d);
-
-		// before measure, get the center of view
-		xPosition = (int) getWidth() / 2;
-		yPosition = (int) getWidth() / 2;
 
 		buttonRadius = (int) (d / 2 * 0.25);
 		joystickRadius = (int) (d / 2 * 0.75);
 	}
 
 	private int measure(int measureSpec) {
-		int result = 0;
+		int result;
 
-		// Decode the measurement specifications.
+		//Decode the measurement specifications.
 		int specMode = MeasureSpec.getMode(measureSpec);
 		int specSize = MeasureSpec.getSize(measureSpec);
 
 		if (specMode == MeasureSpec.UNSPECIFIED) {
-			// Return a default size of 200 if no bounds are specified.
+			//Return a default size of 200 if no bounds are specified.
 			result = 200;
 		} else {
-			// As you want to fill the available space
-			// always return the full available bounds.
+			//As you want to fill the available space always return the full available bounds.
 			result = specSize;
 		}
 		return result;
@@ -114,142 +109,102 @@ public class JoystickView extends View implements Runnable {
 
 	@Override
 	protected void onDraw(Canvas canvas) {
-		// super.onDraw(canvas);
-		centerX = (getWidth()) / 2;
-		centerY = (getHeight()) / 2;
+        //inner circle
+        canvas.drawCircle((int) centerX, (int) centerY, joystickRadius / 2, innerCircle);
 
-		// painting the main circle
-		canvas.drawCircle((int) centerX, (int) centerY, joystickRadius,
-				mainCircle);
-		// painting the secondary circle
-		canvas.drawCircle((int) centerX, (int) centerY, joystickRadius / 2,
-				secondaryCircle);
-		// paint lines
-		canvas.drawLine((float) centerX, (float) centerY, (float) centerX,
-				(float) (centerY - joystickRadius), verticalLine);
-		canvas.drawLine((float) (centerX - joystickRadius), (float) centerY,
-				(float) (centerX + joystickRadius), (float) centerY,
-				horizontalLine);
-		canvas.drawLine((float) centerX, (float) (centerY + joystickRadius),
-				(float) centerX, (float) centerY, horizontalLine);
+        //horizontal line
+        canvas.drawLine(
+                (float) (centerX - joystickRadius), (float) centerY,
+                (float) (centerX + joystickRadius), (float) centerY, horizontalLine);
 
-		// painting the move button
+        //vertical line
+        canvas.drawLine(
+                (float) centerX, (float) (centerY - joystickRadius),
+                (float) centerX, (float) (centerY + joystickRadius), verticalLine);
+
+		//move button
 		canvas.drawCircle(xPosition, yPosition, buttonRadius, button);
 	}
 
 	@Override
-	public boolean onTouchEvent(MotionEvent event) {
+	public boolean onTouchEvent(@NotNull MotionEvent event) {
 		xPosition = (int) event.getX();
 		yPosition = (int) event.getY();
-		double abs = Math.sqrt((xPosition - centerX) * (xPosition - centerX)
-				+ (yPosition - centerY) * (yPosition - centerY));
-		if (abs > joystickRadius) {
-			xPosition = (int) ((xPosition - centerX) * joystickRadius / abs + centerX);
-			yPosition = (int) ((yPosition - centerY) * joystickRadius / abs + centerY);
-		}
+        if(limitCircular) {
+            double radialDistance = Math.sqrt((xPosition - centerX) * (xPosition - centerX)
+                    + (yPosition - centerY) * (yPosition - centerY));
+            if (radialDistance > joystickRadius) {
+                xPosition = (int) ((xPosition - centerX) * joystickRadius / radialDistance + centerX);
+                yPosition = (int) ((yPosition - centerY) * joystickRadius / radialDistance + centerY);
+            }
+        }else{
+            xPosition = limitRange(xPosition, (int) centerX-joystickRadius, (int) centerX+joystickRadius);
+            yPosition = limitRange(yPosition, (int) centerY-joystickRadius, (int) centerY+joystickRadius);
+        }
+
 		invalidate();
-		if (event.getAction() == MotionEvent.ACTION_UP) {
-			xPosition = (int) centerX;
-			yPosition = (int) centerY;
+		if (event.getAction() == MotionEvent.ACTION_UP){
+            resetPosition();
 			thread.interrupt();
-			onJoystickMoveListener.onValueChanged(getAngle(), getPower(),
-					getDirection());
+			onJoystickMoveListener.onValueChanged(getPowerX(), getPowerY());
 		}
-		if (onJoystickMoveListener != null
-				&& event.getAction() == MotionEvent.ACTION_DOWN) {
+
+        if (event.getAction() == MotionEvent.ACTION_MOVE && getParent() != null){
+            getParent().requestDisallowInterceptTouchEvent(true);
+        }
+
+
+        if (onJoystickMoveListener != null && event.getAction() == MotionEvent.ACTION_DOWN) {
 			if (thread != null && thread.isAlive()) {
 				thread.interrupt();
 			}
 			thread = new Thread(this);
 			thread.start();
-			onJoystickMoveListener.onValueChanged(getAngle(), getPower(),
-					getDirection());
+			onJoystickMoveListener.onValueChanged(getPowerX(), getPowerY());
 		}
 		return true;
 	}
 
-	private int getAngle() {
-		if (xPosition > centerX) {
-			if (yPosition < centerY) {
-				return lastAngle = (int) (Math.atan((yPosition - centerY)
-						/ (xPosition - centerX))
-						* RAD + 90);
-			} else if (yPosition > centerY) {
-				return lastAngle = (int) (Math.atan((yPosition - centerY)
-						/ (xPosition - centerX)) * RAD) + 90;
-			} else {
-				return lastAngle = 90;
-			}
-		} else if (xPosition < centerX) {
-			if (yPosition < centerY) {
-				return lastAngle = (int) (Math.atan((yPosition - centerY)
-						/ (xPosition - centerX))
-						* RAD - 90);
-			} else if (yPosition > centerY) {
-				return lastAngle = (int) (Math.atan((yPosition - centerY)
-						/ (xPosition - centerX)) * RAD) - 90;
-			} else {
-				return lastAngle = -90;
-			}
-		} else {
-			if (yPosition <= centerY) {
-				return lastAngle = 0;
-			} else {
-				if (lastAngle < 0) {
-					return lastAngle = -180;
-				} else {
-					return lastAngle = 180;
-				}
-			}
-		}
-	}
+    private void resetPosition(){
+        xPosition = (int) centerX + snapX*joystickRadius;
+        yPosition = (int) centerY + snapY*joystickRadius;
+    }
 
-	private int getPower() {
-		return (int) (100 * Math.sqrt((xPosition - centerX)
-				* (xPosition - centerX) + (yPosition - centerY)
-				* (yPosition - centerY)) / joystickRadius);
-	}
+	private int getPowerX() {
+        return (int) (OUTPUT_SCALE * (xPosition - centerX) / joystickRadius);
+ 	}
 
-	private int getDirection() {
-		if (lastPower == 0 && lastAngle == 0) {
-			return 0;
-		}
-		int a = 0;
-		if (lastAngle <= 0) {
-			a = (lastAngle * -1) + 90;
-		} else if (lastAngle > 0) {
-			if (lastAngle <= 90) {
-				a = 90 - lastAngle;
-			} else {
-				a = 360 - (lastAngle - 90);
-			}
-		}
+    private int getPowerY() {
+        return (int) (OUTPUT_SCALE * (centerY - yPosition) / joystickRadius);
+    }
 
-		int direction = (int) (((a + 22) / 45) + 1);
+    public void setSnap(int snapX, int snapY){
+        this.snapX = snapX;
+        this.snapY = snapY;
+        resetPosition();
+        invalidate();
+    }
+    public void setLimitCircular(boolean limitCircular){
+        this.limitCircular = limitCircular;
+        invalidate();
+    }
 
-		if (direction > 8) {
-			direction = 1;
-		}
-		return direction;
-	}
-
-	public void setOnJoystickMoveListener(OnJoystickMoveListener listener,
-			long repeatInterval) {
+    public void setOnJoystickMoveListener(OnJoystickMoveListener listener, long repeatInterval) {
 		this.onJoystickMoveListener = listener;
 		this.loopInterval = repeatInterval;
 	}
 
 	public static interface OnJoystickMoveListener {
-		public void onValueChanged(int angle, int power, int direction);
+		public void onValueChanged(int powerX, int powerY);
 	}
 
+    //TODO is there a better way than sleeping?
 	@Override
 	public void run() {
 		while (!Thread.interrupted()) {
 			post(new Runnable() {
 				public void run() {
-					onJoystickMoveListener.onValueChanged(getAngle(),
-							getPower(), getDirection());
+					onJoystickMoveListener.onValueChanged(getPowerX(), getPowerY());
 				}
 			});
 			try {
@@ -259,4 +214,8 @@ public class JoystickView extends View implements Runnable {
 			}
 		}
 	}
+	
+    private static int limitRange(int value, int min, int max){
+        return(Math.min(Math.max(value, min), max));
+    }
 }
